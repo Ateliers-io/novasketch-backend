bdb082c1bb9cfa5a72b2b413dd98761d4fa58104
NarainBK <bknarains@gmail.com>
2026-02-25 01:05:26 +0530

    remove mongodb-memory-server dependency

--- tests/utils/db_handler.js
+++ tests/utils/db_handler.js
@@ -1,7 +1,4 @@
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-
-let mongo = null;
 
 export const connect = async () => {
     // If we want to use in-memory:
