bba792571959401164e55312b853782e3af26f64
NarainBK <bknarains@gmail.com>
2026-02-25 10:15:35 +0530

    feat: add PATCH endpoint to toggle lock state

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createSession, getSession } from '../controllers/sessionController.js';
+import { createSession, getSession, lockSession } from '../controllers/sessionController.js';
 
 const router = express.Router();
 
 router.post('/', createSession);
 router.get('/:id', getSession);
+router.patch('/:id/lock', lockSession);
 
 export default router;
